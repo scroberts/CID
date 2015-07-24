b2ca8b02fd3c53d0bc226c30fd53fd625a39e4fb
scroberts <scottroberts8314@gmail.com>
2015-07-24 18:00:05 +0900

    User files for creation of configuration index documents

---
+++ NFIRAOS_CID.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import cid
+
+######## CID Specific Information ##############
+
+### NFIRAOS CID Information
+dirpath = r'/Users/sroberts/Box Sync/Python/CID/CID NFIRAOS/'
+CID_coll = ['Collection-10610']
+htmlfile = 'NFIRAOS_CID.htm'
+outroot = 'NFIRAOS_'
+
+### STR CID Information
+# dirpath = r'/Users/sroberts/Box Sync/Python/CID/CID STR/'
+# CID_coll = ['Collection-10610']
+# htmlfile = 'STR_CID.htm'
+# outroot = 'STR_'
+
+######## End of Specific CID Information
+
+cid.make_cid(dirpath, CID_coll, htmlfile, outroot)
+
+
+
